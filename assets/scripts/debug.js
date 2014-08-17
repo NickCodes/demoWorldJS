f6f19747d0b42f2890f20ede4f07f6a8ac825551
Nick Fisher <nick@nickmf.com>
2014-08-17 19:31:42 -0400

    Added skybox to debugScene

--- assets/scripts/debug.js
+++ assets/scripts/debug.js
@@ -22,16 +22,45 @@ var debugObject = function(window){
 	
 	// DEBUG scene settings used for working out the kinks!
 	debugObject.defaultSceneSettings = function(){
+		
+		// Lights
 		world.defaultDirectionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 		world.defaultDirectionalLight.position.set(0, -11, 0 );
 		world.scene.add( world.defaultDirectionalLight );
-		
 		world.defaultDirectionalLight2 = new THREE.DirectionalLight( 0xffffff, 1 );
 		world.defaultDirectionalLight2.position.set(0, 500, 500 );
 		world.scene.add( world.defaultDirectionalLight2 );
-		
+
+		// Skybox
+			// Load the images
+			var urlPrefix = "assets/images/d_skybox/";
+			var urls = [ urlPrefix + "ashcanyon_right.jpg", urlPrefix + "ashcanyon_left.jpg",
+				urlPrefix + "ashcanyon_top.jpg", urlPrefix + "ashcanyon_top.jpg",
+				urlPrefix + "ashcanyon_front.jpg", urlPrefix + "ashcanyon_back.jpg" ];
+			var textureCube = THREE.ImageUtils.loadTextureCube( urls ); 
+
+			var shader = THREE.ShaderLib[ "cube" ];
+			shader.uniforms[ "tCube" ].value = textureCube;
+
+			var material = new THREE.ShaderMaterial( {
+
+				fragmentShader: shader.fragmentShader,
+				vertexShader: shader.vertexShader,
+				uniforms: shader.uniforms,
+				depthWrite: false,
+				side: THREE.BackSide
+
+			} ),
+
+			mesh = new THREE.Mesh( new THREE.BoxGeometry( 10000, 10000, 10000 ), material );
+			world.scene.add( mesh );
+			
+			
+		// Load additional scene geometry exported from Blender
 		debugObject.debugScene = geometryManager.loadModelJSON('assets/models/scene.js');
 		
+		// Manually positioning the player
+		// TODO - remove this and also adjust the pointerlockcontrols class so it doesn't enforce an arbitrary y=10 clamp
 		cameraManager.camera.position.y = -8;
 	};
 
